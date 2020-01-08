8e86c12fbdb1734fb2984a8e4379973c4d67a76b
Nmuta Jones <nmuta.jones@charter.com>
2020-01-07 20:35:21 -0700

    Add cars JSON response

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -3,7 +3,9 @@ import { Controller, Get, Req, Request} from '@nestjs/common';
 @Controller('cars')
 export class CarsController {
   @Get()
-  findAll(@Req() request: Request): string {
-    return 'this will eventually return a car collection';
+  findAll(@Req() request: Request): {} {
+    return [{make: 'honda', model: 'accord'},
+            {make: 'subaru', model: 'outback'},
+            {make: 'fiat', model: '123 spider'}];
   }
 }
